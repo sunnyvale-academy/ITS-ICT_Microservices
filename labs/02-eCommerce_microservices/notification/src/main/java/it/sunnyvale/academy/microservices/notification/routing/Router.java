bb13bd1536b0b38f7e9b648fd17052318e67e710
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2020-12-14 01:32:17 +0100

    Notification service

---
+++ labs/02-eCommerce_microservices/notification/src/main/java/it/sunnyvale/academy/microservices/notification/routing/Router.java
@@ -0,0 +1,2 @@
+package it.sunnyvale.academy.microservices.notification.routing;public class Router {
+}
---
+++ labs/02-eCommerce_microservices/order/src/main/java/it/sunnyvale/academy/microservices/order/services/NotificationClient.java
@@ -0,0 +1,2 @@
+package it.sunnyvale.academy.microservices.order.services;public class NotificationClient {
+}
