37d8ff31c57bfd48e9bf207e8f13248540102534
Denis <denis.maggiorotto@sunnyvale.it>
2021-01-06 23:07:29 +0000

    fix

--- labs/02-eCommerce_microservices/customer/src/test/java/it/sunnyvale/academy/microservices/customer/CustomerRepositoryUnitTest.java
+++ labs/02-eCommerce_microservices/customer/src/test/java/it/sunnyvale/academy/microservices/customer/CustomerRepositoryUnitTest.java
@@ -11,12 +11,15 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 @TestPropertySource(locations="classpath:application-test.properties")
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 public class CustomerRepositoryUnitTest {
 
     @Autowired
