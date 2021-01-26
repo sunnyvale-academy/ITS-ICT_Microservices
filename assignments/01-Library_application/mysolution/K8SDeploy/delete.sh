39c342d996628227027175429f662d923fe59dd5
FilippoSavarese <59685402+FilippoSavarese@users.noreply.github.com>
2021-01-26 12:19:55 +0100

    Fix

--- assignments/01-Library_application/mysolution/K8SDeploy/delete.sh
+++ assignments/01-Library_application/mysolution/K8SDeploy/delete.sh
@@ -2,3 +2,5 @@ kubectl delete cm,deploy,pod,svc -l app=notification
 kubectl delete cm,deploy,pod,svc -l app=book
 kubectl delete cm,deploy,pod,svc -l app=borrow
 kubectl delete cm,deploy,pod,svc -l app=customer
+
+36740012
